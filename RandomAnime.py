1417ac0b46a00f933ba2ed6d22cd7ceec80d8403
AubreezyR <alrobinson4@dons.usfca.edu>
2022-07-18 13:28:24 -0700

    Finished random anime part of websute 'without styling'

--- RandomAnime.py
+++ RandomAnime.py
@@ -1,17 +1,43 @@
+import random
 import kitsupy
-import requests
-from ranimegen.animegen import RandomAnime
+from flask import Flask, request, render_template
+
+# def getGerne(animeID):
+#     print(animeID)
+#     jikan = Jikan()
+#     suggestedGenre = []
+#     for items in jikan.anime(animeID)['genres']:
+#         suggestedGenre.append(items['name'])
+#     return suggestedGenre
+app = Flask(__name__)
 
 
-async def randzAnime():
-    generator = RandomAnime()
-    suggestion = generator.suggestanime()
-    return suggestion
+@app.route('/')
+def randzAnime():
+    try:
+
+        rand = random.randint(1, 50000)
+        suggest = kitsupy.get_info('anime', rand)['titles']['en']
+        suggestRating = kitsupy.get_info('anime', rand)['averageRating']
+        videoID = kitsupy.get_info('anime', rand)["youtubeVideoId"]
+        if videoID == "":
+            trailer = "No trailer available"
+        else:
+            trailer = "https://www.youtube.com/embed/" + videoID
+
+        # suggestImg = kitsupy.get_info('anime', rand)['posterImage']['tiny']
+
+        return render_template("randomAnime.html", anime="Anime: " + suggest,
+                               rating="\nRating: " + suggestRating + "/100", trailer=trailer)
+    except:
+        return randzAnime()
 
 
-def animeSearch():
-    return kitsupy.search('anime', 'naruto')
+# def animeSearch(anime):
+#     return kitsupy.search('anime', anime)
 
 
 if __name__ == '__main__':
-    print(randzAnime())
+    # print("Generating random anime...")
+    # print(randzAnime())
+    app.run(debug=True)
