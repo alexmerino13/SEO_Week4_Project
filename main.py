7b564f68742c1fae9251cd5bc0e4eb20964b639d
Ian Yeaton <ian.yeaton@gmail.com>
2022-07-22 15:37:17 +0000

    style fixes

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 @app.route("/home")
 def home():
     return render_template('home.html', subtitle='Home Page',
-                         text='This is the home page')
+                            text='This is the home page')
 
 
 @app.route("/menu")
 def about():
-    return render_template('menu.html', subtitle='Menu', text='This is a menu page')
+    return render_template('menu.html', subtitle='Menu',
+                            text='This is a menu page')
 
 
 @app.route('/RandomAnime')
@@ -41,7 +42,7 @@ def randAnime():
 # suggestImg = kitsupy.get_info('anime', rand)['posterImage']['tiny']
 
         return render_template("randomAnime.html", anime="Anime: " + suggest,
-                                rating="\nRating: " + suggestRating + "/100", trailer=trailer)
+                            rating="\nRating: " + suggestRating + "/100", trailer=trailer)
     except:
         return randAnime()
 
@@ -67,7 +68,7 @@ def read():
     randStory = nyt.return_random_story(stories)
     popularstory = nyt.get_popular_stories()
     return render_template("randomArticle.html", rand_url="Random Story url: " + randStory[0],
-                            rand_story="Abstract: " + randStory[1], pop_url="Popular Story url: " + popularstory[0], 
+                            rand_story="Abstract: " + randStory[1], pop_url="Popular Story url: " + popularstory[0],
                             pop_story="Abstract: " + popularstory[1])
 
 
