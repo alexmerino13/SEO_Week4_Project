f9454486baec1dd7acf535414fec67b4bd7967a1
Ian Yeaton <ian.yeaton@gmail.com>
2022-07-22 14:53:57 +0000

    More tests!

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,33 @@
 def test_index(app, client):
     res = client.get('/')
     assert res.status_code == 200
+
+
+def test_home(app, client):
+    res = client.get('/home')
+    assert res.status_code == 200
+
+
+def test_joke(app, client):
+    res = client.get('/Joke')
+    assert res.status_code == 200
+
+
+def test_anime(app, client):
+    res = client.get('/RandomAnime')
+    assert res.status_code == 200
+
+
+def test_read(app, client):
+    res = client.get('/read')
+    assert res.status_code == 200
+
+
+def test_facts(app, client):
+    res = client.get('/funfact')
+    assert res.status_code == 200
+
+
+def test_menu(app, client):
+    res = client.get('/menu')
+    assert res.status_code == 200
